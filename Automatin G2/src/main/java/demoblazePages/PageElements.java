2e0550f0b6e777d76ef2d9976f7f85ddd64f4739
mariam samy <miiro.samy@gmail.com>
2025-04-10 17:22:57 +0200

    Automation Maven Project G2

---
+++ Automatin G2/src/main/java/demoblazePages/PageElements.java
@@ -0,0 +1,46 @@
+package demoblazePages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class PageElements {
+
+    public WebElement getLoginLink(WebDriver driver){
+        return
+                driver.findElement(By.id("login2"));
+    }
+    public WebElement getLoginUsername(WebDriver driver){
+        return
+                driver.findElement(By.id("loginusername"));
+    }
+    public WebElement getLoginPassword(WebDriver driver){
+        return
+                driver.findElement(By.id("loginpassword"));
+    }
+    public WebElement getLoginButton(WebDriver driver){
+        return
+                driver.findElement(By.cssSelector("#logInModal .btn.btn-primary"));
+    }
+    public WebElement getProductImageByIndex(WebDriver driver, int index){
+        List<WebElement> products = driver.findElements(By.className("card-img-top"));
+        return
+                products.get(index);
+    }
+    public WebElement getAddTOCartButton(WebDriver driver){
+        return
+                driver.findElement(By.className("btn-success"));
+    }
+    public WebElement getViewCartButton(WebDriver driver){
+        return
+                driver.findElement(By.id("cartur"));
+    }
+    public WebElement getHomeLink(WebDriver driver){
+        return
+                driver.findElement(By.className("nav-link"));
+    }
+}
+
+
